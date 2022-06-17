ba63b4ff77ae37ba90166e1849289381f8bed122
Stephen Salem Ibitomi <38494567+peter7506@users.noreply.github.com>
2022-06-17 16:12:02 +0000

    Add files via upload

---
+++ scrim - Copy.js
@@ -0,0 +1,6 @@
+function generatecat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://placekitten.com/g/200/300"
+    div.appendChild(image);
+}
